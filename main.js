e8fa7ff0c0549df334b7242cbc4a02c7fea4285a
iidarklol <77841200+iidarklol@users.noreply.github.com>
2021-08-01 12:00:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function twitter(){
+    window.location = "https://twitter.com/milkypeachtv"
+}
+ function insta(){
+     window.location = "https://instagram.com/milkypeachtv "
+ }
+ function tiktok(){
+     window.location = "https://tiktok.com/@milkypeachtv"
+ }
